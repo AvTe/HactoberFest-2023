305ace959561ee1bd0377602692b8d01f447337d
Amit Vishwakarma <52880868+AvTe@users.noreply.github.com>
2023-10-26 20:48:56 +0530

    Add files via upload

---
+++ requirement.bash
@@ -0,0 +1,27 @@
+# Step 1: Setting up your environment
+pip install Flask
+
+# Step 2: Create your project directory
+mkdir my_website
+cd my_website
+
+# Step 3: Project Structure
+Here's a simple project structure:
+
+my_website/
+    app.py
+    templates/
+        index.html
+    static/
+        style.css
+        script.js
+
+
+# Step 4: Write the Python code (app.py)
+
+# Step 5: HTML (templates/index.html)
+
+# Step 6: Run your website
+# Run your Flask application by executing python app.py in 
+# your project directory. 
+# Your website should be accessible at http://localhost:5000.
